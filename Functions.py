aa242a8b87a0c7572fed273073f8a23cceacd9d4
IAmMADMAX <75090307+Nilanjit07@users.noreply.github.com>
2022-03-24 16:09:45 +0530

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,10 +1,16 @@
-str = input('Please enter a string : ')
+string=input("Enter string: ")
+def make_dict(x):
+    dictionary = {}
+    for letter in x:
+        dictionary[letter] = 1 + dictionary.get(letter, 0)
+    return dictionary
 def most_frequent(string):
-    d = dict()
-    for key in string:
-        if key not in d:
-            d[key] = 1
-        else:
-            d[key] += 1
-    return d
-print (most_frequent(str))
+    letters = [letter.lower() for letter in string if letter.isalpha()]
+    dictionary = make_dict(letters)
+    result = []
+    for key in dictionary:
+        result.append((dictionary[key], key))
+    result.sort(reverse=True)
+    for count, letter in result:
+        print (letter,":",count, end="   ")
+most_frequent(string)
