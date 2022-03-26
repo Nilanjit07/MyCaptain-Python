b909880e4547106789dac0369a1da9b4faf1e73d
IAmMADMAX <75090307+Nilanjit07@users.noreply.github.com>
2022-03-26 21:10:19 +0530

    Update Area.py

--- Area.py
+++ Area.py
@@ -1,3 +1,8 @@
-r = float(input("Enter radius:"))
-area = (22/7)*r*r
-print("Area of circle is:",area)
+r=float(input("Enter radius: "))
+area=(22/7)*r*r
+print(area)
+dict={'py':"python",'txt':"text",'bin':"binary",'c':"C",'CPP':"C++",'java':"java"}
+f_name=input("Input the Filename: ")
+l=f_name.split('.')
+if l[1] in dict.keys():
+    print("The extention of the file is:",dict[l[1]])
